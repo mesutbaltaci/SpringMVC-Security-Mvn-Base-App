0ed6c90bb2486fb0b13ea248d049aeb655c28744
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-02-28 15:46:24 -0500

    spring mvc and security base package

---
+++ spring-security-demo-02-basic-security/src/main/java/org/ms/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.ms.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+}
---
+++ spring-security-demo-02-basic-security/src/main/java/org/ms/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package org.ms.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
